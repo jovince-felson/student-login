597d0bc8c63d7627c4b6a222c2ad7bd90b26f478
jovince-felson <jovince1415@gmail.com>
2023-12-18 18:23:43 +0530

    this is my

--- student.js
+++ student.js
@@ -1,28 +1,70 @@
-function saveRecord(){
-  const Name = document.getElementById("Nameb").value;
-
-  const Age = document.getElementById("Ageb").value;
-
-  const Gender = document.getElementById("gender").value;
-  gendervalue = "";
+function saveRecord() {
+  var Name = document.getElementById("Nameb").value;
+  var Age = document.getElementById("Ageb").value;
+  var Email = document.getElementById("email").value;
+  var Gender = document.getElementsByName("gender");
+  var genderValue = "";
 
   for (let i of Gender) {
     if (i.checked) {
-      gendervalue = i.value;
+      genderValue = i.value;
     }
   }
+  var Language = document.getElementsByName("language");
 
-  const language = document.getElementById("Language").value;
+  var languageArray = [];
 
-  var lanugeArray = [];
-
-  for (let i of language) {
+  for (let i of Language) {
     if (i.checked) {
-      lanugeArray.push(i.value);         
+      languageArray.push(i.value);
     }
   }
 
-  const Email = document.getElementById('email').value
+  // console.log(Name);
+  // console.log(Age);
+  // console.log(Email);
+  // console.log(genderValue);
+  // console.log(languageArray);
 
-  
+  var row = document.createElement("tr");
+
+var Nametd = document.createElement("td");
+Nametd.textContent = Name;
+
+row.append(Nametd);
+
+var Agetd = document.createElement('td')
+Agetd.textContent=Age
+
+row.append(Agetd)
+
+var gendertd = document.createElement('td')
+gendertd.textContent=Gender
+
+row.append(gendertd)
+
+var languagetd = document.createElement('td')
+languagetd.textContent = Language
+row.append(languagetd)
+
+var emailtd = document.createElement('td')
+emailtd.textContent = Email
+row.append(emailtd)
+
+var deletebtn = document.createElement('td')
+deletebtn.textContent='Delete'
+
+deletebtn.setAttribute('onclick',"deleteRow()")
+row.append(deletebtn)
+
+// console.log(row)
+
+var Table = document.querySelector('#recordTable')
+Table.append(row)
+
+
+
+}
+function deleteRow(){
+  console.log('delete');
 }
